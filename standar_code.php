29a852f890c491ec0a7b52502b2997ede819bbb1
hanumfnr <hanumfaulinnuur@gmail.com>
2023-07-10 12:43:01 +0700

    pertemuan9

---
+++ standar_code.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'koneksi.php';
+include 'koneksi.php';
+
+$movie = array (
+    '1' => 'film A',
+    '2' => 'film B',
+    '3' => 'film C',
+    '4' => 'film D',
+);
+
+function selamatdatang (){
+
+}
+
+function get_nama () {
+    return $nama;
+}
+
+
+
+
+?>
